1b87dd36347d5226639c300d0b7a8b468f45855e
Pranavpaudel <pranav.paudel.123@gmail.com>
2025-11-03 19:18:28 +0545

    guest route updated

--- routes/guestRoutes.js
+++ routes/guestRoutes.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-//guest routes 
-router.get('/articles', (req, res) => {
-  res.send('List of public articles');
-});
+// Import guest controller functions
+const {
+  getHomePage,
+  getBlogPosts,
+  getSinglePost,
+  getContactPage,
+  getAboutPage
+} = require('../controllers/guestController');
 
-router.get('/articles/:id', (req, res) => {
-  const articleId = req.params.id;
-  res.send(`Viewing article with ID: ${articleId}`);
-});
+// Public routes for blog visitors
+router.get('/', getHomePage); // Homepage
+router.get('/posts', getBlogPosts); // List of blog posts
+router.get('/posts/:id', getSinglePost); // Individual blog post
+router.get('/contact', getContactPage); // Contact page
+router.get('/about', getAboutPage); // About page
 
 module.exports = router;
